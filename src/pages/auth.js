df43615ac7129bc0b3d08f9c74abeac21a62cd59
sknaturalfarm <tech.sknaturalfarm@gmail.com>
2025-09-21 23:00:42 +0530

    Update auth.js

--- src/pages/auth.js
+++ src/pages/auth.js
@@ -267,7 +267,7 @@ export default function AuthPage() {
     setMessage(null);
 
     try {
-      const res = await fetch("http://localhost:8080/api/auth/register", {
+      const res = await fetch("https://sknfauthbackend-production.up.railway.app/api/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ fullName, email, password }),
@@ -298,7 +298,7 @@ export default function AuthPage() {
     setMessage(null);
 
     try {
-      const res = await fetch("http://localhost:8080/api/auth/login", {
+      const res = await fetch("https://sknfauthbackend-production.up.railway.app/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
